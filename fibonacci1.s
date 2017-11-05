ea96180804d4958038c35972c315dd24c64e62d3
mounika-t <33395198+mounika-t@users.noreply.github.com>
2017-11-05 22:53:00 +0530

    Add files via upload

--- fibonacci1.s
+++ fibonacci1.s
@@ -6,18 +6,18 @@
 __main  FUNCTION
 		MOV R0,#0
 		MOV R1,#1
-		MOV R2,#45
+		MOV R2,#1
 		MOV R3,#0X20000000
 		MOV R4,#0
-		ADD R4,R0,R1
-LOOP1  CMP R2,R4
-	    BGT LOOP
+LOOP1   CMP R2,#10
+	    BLT LOOP
 	    B STOP
-LOOP    STR R4,[R3]
+LOOP    ADD R4,R0,R1
+        STR R4,[R3]
         MOV R0,R1
 		MOV R1,R4
-		ADD R4,R0,R1
-		B LOOP1
+		ADD R2,R2,1
+	    B LOOP1
 STOP	B STOP
         ENDFUNC
     END	 
